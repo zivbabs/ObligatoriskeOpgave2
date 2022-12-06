0d4c66ae3d93c615ca699aba42e7b8546465928c
zivbabs <117026503+zivbabs@users.noreply.github.com>
2022-12-06 05:28:04 +0100

    Revert test

--- src/DelfinenSvømmeklub.java
+++ src/DelfinenSvømmeklub.java
@@ -4,6 +4,5 @@ public class DelfinenSvømmeklub {
     public static void main(String[] args) throws FileNotFoundException {
         Ansat a = new Ansat();
         a.login();
-        // fart
     }
 }
